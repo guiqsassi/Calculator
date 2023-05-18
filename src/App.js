cb9177ea742bf738691093e1dfd0644556ded9ff
guiqsassi <guiqsassi@gmail.com>
2023-05-17 22:30:03 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import './App.css'
+  
 function App() {
+  let [display, setDisplay] = useState('')
+  let [display2, setDisplay2] = useState('')
+  let [resultado, setResultado] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <div className="container">
+         <div className='ball'>
+
+  </div>
+   
+      <div className='calculator'>
+          <div className='display'>
+              <h1 className='displayText'>{resultado} </h1>
+              <h1 className='displayText'>{display} </h1>
+          </div>
+          
+
+          <div className='digits'>
+            <button className='buttons'onClick={() =>{ 
+                  setDisplay(display += "% ")
+             }}> %</button>
+            <button className='buttons'onClick={() =>{ setDisplay("") }}> CE</button>
+            <button className='buttons'onClick={() =>{ setDisplay("") }}> C</button>
+            <button className='buttons'onClick={() =>{ setDisplay(display += " - ") }}> -</button>
+            <button className='buttons' onClick={() =>{ setDisplay(display += "7") }}> 7</button>
+            <button className='buttons' onClick={() =>{ setDisplay(display += "8") }}> 8</button>
+            <button className='buttons' onClick={() =>{ setDisplay(display += "9") }}> 9</button>
+            <button className='buttons'onClick={() =>{ setDisplay(display += " / ") }}> / </button>
+            <button className='buttons'onClick={() =>{ setDisplay(display += "4") }}> 4</button>
+            <button className='buttons'onClick={() =>{ setDisplay(display += "5") }}> 5</button>
+            <button className='buttons'onClick={() =>{ setDisplay(display += "6") }}> 6</button>
+            <button className='buttons'onClick={() =>{ setDisplay(display += " * ") }}> X</button>
+            <button className='buttons' onClick={() =>{ setDisplay(display += "1") }}> 1</button>
+            <button className='buttons' onClick={() =>{ setDisplay(display += "2") }}> 2</button>
+            <button className='buttons' onClick={() =>{ setDisplay(display += "3") }}> 3</button>
+            <button className='buttons' onClick={() =>{ setDisplay(display += " + ") }}> +</button>
+            <button className='buttons' onClick={() =>{ setDisplay(display += ".") }}> .</button>
+            <button className='buttons'onClick={() =>{ setDisplay(display += "0") }}> 0</button>
+            <button className='buttons' onClick={() =>{ let resultado = (eval(display)) 
+            setDisplay(resultado)
+            }}> =</button>
+       
+          </div>
+          
+          
+      </div>
+    
     </div>
+    
   );
 }
 
